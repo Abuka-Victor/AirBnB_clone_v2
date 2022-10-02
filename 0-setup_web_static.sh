ce1aafc214fe289f806fd24a6c16606474c00bb7
Abuka-Victor <abukavictoro@gmail.com>
2022-10-03 00:44:50 +0100

    Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,20 +1,34 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
 
-apt-get install -y nginx
+apt install -y nginx
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared
-touch /data/web_static/releases/test/index.html
+echo "<h1>Hello world</h1>" > /data/web_static/releases/test/index.html
 
-ln -s -f /data/web_static/releases/test/ /data/web_static/current
-chown -R $USER:$USER /data
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R $USER /data
+chgrp -R $USER /data/
 
-printf %s "{
-		
-	location /hbnb_static {
-		alias /
-	}
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 https://www.youtube.com/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
-}" >> /ete/nginx/sites-available/default
-
+service nginx restart
