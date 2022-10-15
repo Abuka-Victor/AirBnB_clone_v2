670750a4427a30ecef3eb77f55488ef6cb955674
Abuka-Victor <abukavictoro@gmail.com>
2022-10-15 21:14:42 +0100

    Task 5 fix templating format

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -45,7 +45,7 @@ def display_num(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def display_temp(n):
     """ Return a template """
-    return render_template("templates/5-number.html", number=n)
+    return render_template("5-number.html", number=n)
 
 
 if __name__ == "__main__":
