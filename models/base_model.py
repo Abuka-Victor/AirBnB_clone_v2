4b493995fa07faa7bdcd64e4db9ca60d5a6bec02
Abuka-Victor <abukavictoro@gmail.com>
2022-10-16 00:03:08 +0100

    Base model tweaks

--- models/base_model.py
+++ models/base_model.py
@@ -22,6 +22,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            print(self.created_at)
+            print(self.updated_at)
         else:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
