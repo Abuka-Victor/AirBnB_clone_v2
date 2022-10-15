34a42245d6c7384c3c5bdb2d6ae2441da016380f
Abuka-Victor <abukavictoro@gmail.com>
2022-10-16 00:47:24 +0100

    Task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,9 +12,11 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def state_list():
     list_of_states_objects = storage.all(State)
+    print(list_of_states_objects)
     list_of_states = []
     for key, value in list_of_states_objects.items():
         list_of_states.extend([value.id, value.name])
+    print(list_of_states)
     return render_template('7-states_list.html', states=list_of_states)
 
 
