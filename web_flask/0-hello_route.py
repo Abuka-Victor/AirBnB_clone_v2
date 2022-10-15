7fa0e10ee9f3db0b2933201bd29f18ee0b45f5ec
Abuka-Victor <abukavictoro@gmail.com>
2022-10-15 19:50:12 +0100

    task 0 fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,12 @@
+#!/usr/bin/python3
+"""
+A simple flask script to start up an app
+"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """ Return hello hbnb"""
     return "Hello HBNB!"
